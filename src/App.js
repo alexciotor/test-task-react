b94bf1b0e6afb7a756eb49053089423f297e3864
alexciotor <ciotor.alex@yahoo.com>
2021-02-09 02:28:02 +0200

    test deplou

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import {FaUsers} from 'react-icons/fa'
+import {HiClipboardList} from 'react-icons/hi'
+import {ImMenu} from 'react-icons/im'
+import {FiPlay} from 'react-icons/fi'
+import {btn} from './modules/data'
+import {content} from './modules/data'
+import './App.scss';
+import React , {useState} from 'react'
+
+
+function App() {
+  let key = new Date().getTime().toString()
+  const [value, setValue] = useState(0)
+  return (
+    <section className='main-section' >
+    <div className="left-menu">
+    <ul className="ul-list">
+    <li className="menu-item"><ImMenu/>
+ 
+    </li>
+    <li className="menu-item"><HiClipboardList/></li>
+    <li className="menu-item"><FaUsers/></li>
+    </ul>
+    </div>
+    <div className="right-content">
+    <nav className="nav-bar">
+    <div className="nav-content">
+    <span className="courses">courses</span>
+    <ul className="nav-list">
+      {btn.map((btn,index)=>{
+        const{name,id} =btn
+       return <li key={id} className="nav-li-item "><button className="btn" onClick={()=>{
+         setValue(index)
+       }} > {name}</button> {value===index && <div className="underline"></div>}</li>
+    })}
+     
+    </ul>
+    </div>
+    </nav>
+
+    <div className="main-content">
+ {content.map((item,index)=>{
+   const {name,les,time,img} = item
+   return <article key={index} className="article">
+   <img className='img' src={img} alt={name}/>
+   <div className="text-content">
+   <div className="title">
+   <p className='name' >{name}</p> 
+   <p className='les' >{les} lessons </p>
+   </div>
+   <span className='time' >{time} min</span>
+  
+   </div>  
+    <span className="play"><FiPlay/></span>
+   </article>
+ })}
+    
+    </div>
+
+    </div>
+ 
+    </section>
+  );
+}
+
+export default App;
---
+++ src/modules/data.js
@@ -0,0 +1,33 @@
+export  const btn =[
+    {
+        name:'Popular',
+        id:0,
+},
+{
+    name:'favorite',
+    id:1,
+},
+{
+    name:'new',
+    id:2
+}
+
+]
+
+
+
+
+export const content = [{
+    name: 'ilustration',
+    les:24,
+    time:134,
+     img:'https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1709&q=80'
+},
+{
+    name:'Graphic design',
+    les: 30,
+    time: 236,
+    img:  'https://www.talentedladiesclub.com/site/wp-content/uploads/Why-you-should-choose-custom-modular-structures-for-permanent-and-portable-projects.jpg'
+
+}
+]
